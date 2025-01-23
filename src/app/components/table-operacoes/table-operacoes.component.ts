4afc314ca8f08d87edc9e884489cc5d5dc8a2881
Samanta Barros <samantadesenvolvedora@gmail.com>
2025-01-23 17:54:30 -0300

    feat: Dados dinâmicos adicionados a tabela de operações

--- src/app/components/table-operacoes/table-operacoes.component.ts
+++ src/app/components/table-operacoes/table-operacoes.component.ts
@@ -1,29 +1,41 @@
 import { Component } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
+import { GetDadosService } from '../../services/get-dados.service';
+import { CommonModule } from '@angular/common';
 
 export interface DadosOperacao {
   operacao: string
-  nomeResponsavel: string,
+  responsavel: string,
   data: string,
   local: string,
   veiculos: string[],
   status: string
 }
 
-const ELEMENT_DATA: DadosOperacao[] = [
-  {operacao: '001', nomeResponsavel: 'Priscila Farias Souza', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro A','Carro B', 'Carro C'], status: 'Finalizada'},
-  {operacao: '002', nomeResponsavel: 'Marcos Paiva Santos', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro A'], status: 'Em andamento'},
-  {operacao: '003', nomeResponsavel: 'Caio Costa Pinheiro', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro C'], status: 'Finalizada'},
-  {operacao: '004', nomeResponsavel: 'Pedro Lima Barreto', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro D'], status: 'Finalizada'},
-]
+// const ELEMENT_DATA: DadosOperacao[] = [
+//   {operacao: '001', responsavel: 'Priscila Farias Souza', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro A','Carro B', 'Carro C'], status: 'Finalizada'},
+//   {operacao: '002', responsavel: 'Marcos Paiva Santos', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro A'], status: 'Em andamento'},
+//   {operacao: '003', responsavel: 'Caio Costa Pinheiro', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro C'], status: 'Finalizada'},
+//   {operacao: '004', responsavel: 'Pedro Lima Barreto', data: '22/01/2024', local: 'Vitória da Conquista', veiculos: ['Carro D'], status: 'Finalizada'},
+// ]
 
 @Component({
   selector: 'app-table-operacoes',
-  imports: [MatTableModule],
+  imports: [MatTableModule, CommonModule],
   templateUrl: './table-operacoes.component.html',
   styleUrl: './table-operacoes.component.scss'
 })
 export class TableOperacoesComponent {
-  columnsToDisplay: string[] = ['operacao', 'nomeResponsavel', 'data', 'local', 'veiculos', 'status']
-  dadosOperacao = ELEMENT_DATA;
+  columnsToDisplay: string[] = ['operacao', 'responsavel', 'data', 'local', 'veiculos', 'status']
+  dadosOperacao = [];
+
+  constructor(private dadosService: GetDadosService){}
+
+  ngOnInit(): void {
+    this.dadosService.getDadosOperacoes().subscribe((data) => {
+      this.dadosOperacao = data;
+      console.log(this.dadosOperacao);
+    });
+  }
 }
+
--- src/app/pages/operacoes/operacoes.component.ts
+++ src/app/pages/operacoes/operacoes.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { TableOperacoesComponent } from '../../components/table-operacoes/table-operacoes.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-operacoes',
-  imports: [MatIconModule,  TableOperacoesComponent],
+  imports: [MatIconModule,  TableOperacoesComponent, MatCardModule, MatToolbarModule],
   templateUrl: './operacoes.component.html',
   styleUrl: './operacoes.component.scss'
 })

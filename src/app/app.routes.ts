94d9f1dec9758957b188b0ffad0433ba2fc83f69
samantabarros <samantadesenvolvedora@gmail.com>
2025-05-31 21:36:13 -0300

    (change): ajustar rotas e adicionar importação tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { UsuarioAutenticadoGuard } from './services/guards/usuario-autenticado.guard';
 import { UsuarioNaoAutenticadoGuard } from './services/guards/usuario-nao-autenticado.guard';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/home-niveis/home.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { RecuperacaoComponent } from './pages/recuperacao/recuperacao.component';
@@ -14,42 +14,37 @@ export const routes: Routes = [
     {
         path: "inicio",
         component: InicioComponent,
-        canActivate: [UsuarioNaoAutenticadoGuard]
 
     },
 
     {
         path: "login",
         component: LoginComponent,
-        canActivate: [UsuarioNaoAutenticadoGuard]
 
     },
 
     {
         path: "cadastro",
         component: CadastroComponent,
-        canActivate: [UsuarioNaoAutenticadoGuard]
     },
     {
         path: "recuperacao",
         component: RecuperacaoComponent,
-        canActivate: [UsuarioNaoAutenticadoGuard]
     },
     {
         path: "informacao",
         component: InformacaoComponent,
-        canActivate: [UsuarioNaoAutenticadoGuard]
     },
     {
-        path: "",
-        component: MainLayoutComponent,
+        path: "home",
+        component: HomeComponent,
         canActivate: [UsuarioAutenticadoGuard],
-        children: [
+        /*children: [
             {
                 path:"home",
                 component: HomeComponent
             },
-        ]
+        ]*/
     },
 
 ];

586fd49882d750675e2b2da22319bde7fec5df29
samantabarros <samantadesenvolvedora@gmail.com>
2025-06-02 11:41:38 -0300

    fix: ajustar navegação entre as partes da tela de informação

--- src/app/pages/informacao/informacao.component.ts
+++ src/app/pages/informacao/informacao.component.ts
@@ -10,21 +10,12 @@ import { Router } from '@angular/router';
 })
 export class InformacaoComponent {
   parteAtual = 0;
+  informacoes = 4;
 
   constructor (private router: Router) {}
-  informacoes = [
-        {1: "Gidevaldo Novais dos Santos"}, 
-        {2: "Álisson Natan dos Anjos Pinheiro "},
-        {3: "Camille Rodrigues Costa "},
-        {4: "Cleiton Galvão Santana "},
-        {5: "Larissa Ribeiro Firminio "},
-        {6: "Rafaela Pereira Santos (Scrum Master + Designer UX/UI)"},
-        {7: "Samanta Souza Barros (Dev Frontend)"},
-        {8: "Sanley Pires Ferreira (Analista de Qualidade - QA)"}, 
-  ];
-
+ 
   proximaParte() {
-    if(this.parteAtual < this.informacoes.length - 1){
+    if(this.parteAtual <= this.informacoes){
       this.parteAtual++;
     }
   }
@@ -37,7 +28,7 @@ export class InformacaoComponent {
 
   navigateInfo() {
     const rotaAtual = this.router.url;
-    if(rotaAtual === '/inicio'){
+    if(rotaAtual === '/inicio' ){
     this.router.navigate(["informacao"])
     }else if (rotaAtual === '/informacao'){
     this.router.navigate(["inicio"])
